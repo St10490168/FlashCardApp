ab19da847f244c889a766a3d90ad679eae43a2b6
Yannick Ndimba <st10490168@vcconnect.edu.za>
2025-05-13 18:05:36 +0200

    Created second screen layout

---
+++ app/src/main/java/vcmsa/ci/flashcardapp/FlashCardApp2.kt
@@ -0,0 +1,20 @@
+package vcmsa.ci.flashcardapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FlashCardApp2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_flash_card_app2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
