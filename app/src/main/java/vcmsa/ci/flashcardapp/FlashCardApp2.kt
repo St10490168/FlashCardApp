50830efc223da9b5a5ec5a0c3f1dcafe4fd9b673
Yannick Ndimba <st10490168@vcconnect.edu.za>
2025-05-18 19:54:26 +0200

    fixing programming logic

--- app/src/main/java/vcmsa/ci/flashcardapp/FlashCardApp2.kt
+++ app/src/main/java/vcmsa/ci/flashcardapp/FlashCardApp2.kt
@@ -1,15 +1,17 @@
 package vcmsa.ci.flashcardapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class FlashCardApp2 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        private val  questions = arrayOf(
+        val  questions = arrayOf(
             "Did the french revolution end in 1799?" +
                     "Is Donald trump the 31st President of the USA?" +
                     "Nelson Mandela was President in 1994?" +
@@ -17,16 +19,68 @@ class FlashCardApp2 : AppCompatActivity() {
                     "North America won the Vietnam War?"
         )
         val answers = booleanArrayOf(true,false,true,true,false)
-        var = 0
-        var  currentQuestionIndex = 0
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_flash_card_app2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        
+        var score = 0
+        var currentQuestionIndex = 0
+        
+        lateinit var tvQuestion: TextView
+        lateinit var trueButton: Button
+        lateinit var falseButton: Button
+        lateinit var nextButton: Button
+        lateinit var tvAnswer: TextView
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_flash_card_app2)
+
+        questions = findViewById(tvQuestion)
+        falseButton = findViewById(R.id.btnFalse)
+        trueButton = findViewById(R.id.btnTrue)
+        nextButton = findViewById(R.id.btnNext)
+        answers = findViewById(R.id.tvAnswer)
+
+        
+        displayQuestion()
+        
+        trueButton.setOnClickListener { 
+            checkAnswer(
+                true,
+                userAnswer = TODO()
+            )
+        }
+        falseButton.setOnClickListener { 
+            checkAnswer(
+                false,
+                userAnswer = TODO()
+            )
+        }
+        nextButton.androidx.compose.foundation.layout.Box {
+            setOnClickListener {
+                currentQuestionIndex
+                if (currentQuestionIndex < questions.size) {
+                    tvAnswer()
+                } else {
+                    val intent = Intent(this,AppCompatActivity::class.java)
+                    intent.putExtra("score", score)
+                    startActivity(intent)
+                    finish()
+                }
+
+
+            }
+        }
+
+
+    fun displayQuestion() {
+        tvQuestion.text = questions[currentQuestionIndex]
+    }
+
+    fun checkAnswer(userAnswer: Boolean) {
+        if (userAnswer == answers[currentQuestionIndex]) {
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show()
+            score++
+        } else {
+            Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show()
         }
     }
 }
